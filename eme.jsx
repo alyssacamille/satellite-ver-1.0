eef38dc94be7bdbe8af1d68d4e086f192057f138
alyssacamille <alyssacamilleschool2022@gmail.com>
2024-03-29 05:14:18 -0700

    .gitignore trial
    donmai

---
+++ eme.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function eme() {
+  return (
+    <div>
+      <> nasama ba to, i unselecrt ko</>
+      <> alisin ko sha sa .gitignore</>
+    </div>
+  )
+}
+    
