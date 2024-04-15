aaadab42f2c86010bd43c0d71b85a0dbfbf1e91a
alyssacamille <143060589+alyssacamille@users.noreply.github.com>
2024-04-14 20:27:37 -0700

    Update Logo.jsx

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 // import './Logo.css'
+import Logo from '../assets/satellite-logo.svg'
 
 export default function Logo() {
   // XD why are there 2 divs with no css or existence wrapping up an image 😂😂
